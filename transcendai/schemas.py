6729e89f0d902b0f93c2417b435ef42fee69af3c
liortct <liortct@gmail.com>
2024-10-26 22:52:29 +0300

    feat: changed default value of max size to be unlimitted.

--- transcendai/schemas.py
+++ transcendai/schemas.py
@@ -7,7 +7,7 @@ class SummarizeRequest(BaseModel):
     text: str
     translate: bool = True  # Flag to translate Hebrew text to English and also repose
     temperature: float = 1.0  # Default is 1.0 for neutral randomness
-    max_length: int = 128  # Default maximum length of tokens generated
+    max_length: int = -1  # Default maximum length of tokens generated
     top_p: float = 1.0  # Default to 1.0 (no nucleus sampling)
     top_k: int = 50  # Default to 50 for balanced generation
     repetition_penalty: float = 1.0  # Default to no repetition penalty
